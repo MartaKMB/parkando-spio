d05fa244ea31c1757d52230e85a225486f4da844
MartaMucha <marta.mucha.balcerek@rsqtechnologies.com>
2019-05-25 15:08:39 +0200

    added style for confirmation page

--- src/components/ConfirmationPage/ConfirmationPage.js
+++ src/components/ConfirmationPage/ConfirmationPage.js
@@ -3,15 +3,44 @@ import React, { Component } from 'react';
 import { withRouter } from "react-router-dom";
 
 class ConfirmationPage extends Component {
+    state = {
+        confirm: false,
+    }
+
+    setConfirm = () => {
+        this.setState({
+          confirm: true
+        })
+      }
+
     render() {
         return (
-            <section>
-                Użytkownik z kartą o numerze: {this.props.match.params.userId} ma przypisane miejsce: {this.props.match.params.parkingId}
-                <button
-                    onClick={(e) => {e.preventDefault(); this.props.history.push(`/`)}}
-                >
-                    WYLOGUJ
-                </button>
+            <section className="confirmation-container">
+                Wybrałeś miejsce:
+                <div className="confirmation-container__number">{this.props.match.params.parkingId}</div>
+                {
+                    this.state.confirm ?
+                        <button
+                            className="buttons-section__log-out-btn"
+                            onClick={(e) => {e.preventDefault(); this.props.history.push(`/`)}}
+                        >
+                            WYLOGUJ
+                        </button>
+                        : <div className="confirmation-container__buttons-section buttons-section">
+                            <button
+                                className="buttons-section__log-out-btn"
+                                onClick={this.setConfirm}
+                            >
+                                ZATWIERDŹ
+                            </button>
+                            <button
+                                className="buttons-section__back-btn"
+                                onClick={(e) => {e.preventDefault(); this.props.history.push(`/choicePaking/${this.props.match.params.userId}`)}}
+                            >
+                                COFNIJ
+                            </button>
+                        </div>
+                }
             </section>
         )
     }
